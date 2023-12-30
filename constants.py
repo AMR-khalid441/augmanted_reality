6d5ec8c2a3250b712ad3fc41dc872ea49f13ecd7
amr khalid <khalidamr665@gmail.com>
2023-12-31 00:03:47 +0200

    Initial commit

---
+++ constants.py
@@ -0,0 +1,13 @@
+# Shape constants
+SHAPE_CONE = "cone"
+SHAPE_SPHERE = "sphere"
+
+# Example usage
+selected_shape = SHAPE_CONE
+
+if selected_shape == SHAPE_CONE:
+    print("Selected shape is a cone.")
+elif selected_shape == SHAPE_SPHERE:
+    print("Selected shape is a sphere.")
+else:
+    print("Unknown shape.")
